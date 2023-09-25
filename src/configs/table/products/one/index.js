e4ff56fc3ee548f7a533cf0ffaf5711fbd87536d
thatsrohitnaik <rohit24naik@gmail.com>
2023-09-25 15:59:10 +0530

    2 files modified

--- src/configs/table/products/one/index.js
+++ src/configs/table/products/one/index.js
@@ -12,7 +12,9 @@ const oneCol = [
   { field: 'movies', cellEditor: 'agNumberCellEditor', editable: true },
   { field: 'latestMovie', cellEditor: 'agTextCellEditor' },
   { field: 'upcomingMovie', cellEditor: 'agTextCellEditor', editable: true },
-  { field: 'price' },
+  { field: 'homeTown' },
+  { field: 'followers', cellEditor: 'agNumberCellEditor', editable: true },
+  { field: 'pilotLicense' },
 ];
 
 export default oneCol;
